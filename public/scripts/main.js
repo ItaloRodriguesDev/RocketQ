8241d6d309ee74f02b10a665c07228db579168dd
Italo Rodrigues <italorodriguesxxii@gmail.com>
2022-05-26 21:44:20 -0300

    Atualização main

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,26 +3,24 @@ import Modal from './modal.js'
 const modal = Modal()
 
 const modalTitle = document.querySelector('.modal h2')
-const modalDescription = document.querySelector('modal p')
+const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
 
 //Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
-  //Adicionar escuta
-  button.addEventListener('click', event => {
-    button.addEventListener('click', handleClick)
-  })
+  //adicionar a escuta
+  button.addEventListener('click', handleClick)
+})
 
-/*Quando o botao delete for clicado ele abre o modal*/
+/*Quando o botão delete for clicado ele abre a modal */
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
   button.addEventListener('click', handleClick)
 })
 
-function handleClick() {
-  //Abrir modal
+function handleClick(event) {
   modal.open()
 }
