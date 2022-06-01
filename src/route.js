a6a7a36bdcab0f8dff7af52de590275f35928f15
Italo Rodrigues <italorodriguesxxii@gmail.com>
2022-06-01 18:11:49 -0300

    Adicionamos o Controller

--- src/route.js
+++ src/route.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
+const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
-route.post('/room/:room/:question/:check/:action')
+route.post('/room/:room/:question/:check/:action', QuestionController.index)
 
 module.exports = route
