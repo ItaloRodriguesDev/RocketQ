667ea5658057edce2be1175e28bdab567ad46aee
Italo Rodrigues <italorodriguesxxii@gmail.com>
2022-06-03 13:11:11 -0300

    ALteração no main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -29,7 +29,7 @@ function handleClick(event, check = true) {
   const form = document.querySelector('.modal form')
   const questionId = event.target.dataset.id
 
-  form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
+  form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`)
 
   modalTitle.innerHTML = `${text} esta pergunta`
   modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
