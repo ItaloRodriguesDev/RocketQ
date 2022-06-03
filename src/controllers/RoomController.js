15f78bbecb2f980edcd054c31c8f22dbbaee11fb
Italo Rodrigues <italorodriguesxxii@gmail.com>
2022-06-03 13:12:41 -0300

    res.redirect

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,3 +1,8 @@
 module.exports = {
-  create() {}
+  create(req, res) {
+    let roomId = 123456
+
+    //res.render para redirecionar para a rota, passando para o Id com as aspas "/room/", tambem usei template string.
+    res.redirect(`room/${roomId}`)
+  }
 }
